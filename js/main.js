7479df3c35dbff286445c1dbee59c01b3f15d93e
Robert Ableson <robertableson@gmail.com>
2022-05-31 11:46:29 -0400

    Added CORS support + API test call.

--- js/main.js
+++ js/main.js
@@ -6,7 +6,21 @@ class Coin{
         const result = 'heads' //test result 
         const domElement_resultDisplay = document.querySelector('#result-display')
 
+        this.callApi()
+
         domElement_resultDisplay.innerHTML = `You flipped: ${result}`
+    }
+
+    async callApi(){
+        try{
+            const response = await fetch('http://localhost:8000/api/heads-or-tails')
+            const data = await response.json()
+    
+            console.log(data)
+            //document.querySelector('h2').innerText = data.birthName
+        }catch(error){
+            console.log(error)
+        }
     }
 }
 
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
-//const cors = require('cors')
+const cors = require('cors')
 const PORT = 8000
 
-//app.use(cors())
+app.use(cors())
 
 app.get('/api/heads-or-tails/', (request, response) => {
-    const result = Math.random() >= 0.5 ? 'Heads' : 'Tails'
+    const result = Math.random() >= 0.5 ? 'heads' : 'tails'
     response.json(result)
 })
 
