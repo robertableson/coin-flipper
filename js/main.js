481773629469c2b040905470544fcb57391bce86
Robert Ableson <robertableson@gmail.com>
2022-05-31 12:11:42 -0400

    Added async support and polished class structure.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,35 @@
 //No semicolons for this file 
 
 class Coin{
-    getHeadsOrTails(){
-        //do api call to backend here
-        const result = 'heads' //test result 
-        const domElement_resultDisplay = document.querySelector('#result-display')
+    constructor(){
+        this.initEvents()
+    }
 
-        this.callApi()
+    initEvents(){
+        const domElement_getResultBtn = document.querySelector('#btn-heads-or-tails')
+
+        domElement_getResultBtn.addEventListener('click', () => this.getHeadsOrTails())
+    }
+
+    async getHeadsOrTails(){
+        const result = await this.callApi()
+        const domElement_resultDisplay = document.querySelector('#result-display')
 
         domElement_resultDisplay.innerHTML = `You flipped: ${result}`
     }
-
     async callApi(){
         try{
             const response = await fetch('http://localhost:8000/api/heads-or-tails')
             const data = await response.json()
-    
+
             console.log(data)
-            //document.querySelector('h2').innerText = data.birthName
+
+            return data
         }catch(error){
             console.log(error)
+            return data
         }
     }
 }
 
 let coin = new Coin()
-coin.getHeadsOrTails()
