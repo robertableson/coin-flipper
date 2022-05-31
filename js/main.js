61ed96bf11e83fb9c66f44b1558a7b4a4048fb78
Robert Ableson <robertableson@gmail.com>
2022-05-31 11:00:43 -0400

    Base JS class.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//No semicolons for this file 
+
+class Coin{
+    getHeadsOrTails(){
+        //do api call to backend here
+        
+        console.log('test') 
+    }
+}
+
+let coin = new Coin()
+coin.getHeadsOrTails()
